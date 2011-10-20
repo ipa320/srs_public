00735c525da1ae990e92f231285d39e5fd819375
mrodriguez-robotnik <mrodriguez@robotnik.es>
2011-10-20 10:40:45 +0200

    Updated files

--- srs_grasping/msg_gen/lisp/_package_GraspResult.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package srs_grasping-msg)
-(cl:export '(GRASP_CONFIGURATION-VAL
-          GRASP_CONFIGURATION
-))
--- srs_grasping/src/srs_grasping/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._GraspAction import *
-from ._GraspActionResult import *
-from ._GraspResult import *
-from ._GraspGoal import *
-from ._GraspActionGoal import *
-from ._GraspFeedback import *
-from ._GraspActionFeedback import *

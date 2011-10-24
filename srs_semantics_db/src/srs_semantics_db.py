bfe5d0b963ac4c85da6a44e99e9a9ac54b4660a6
jizecn <jizecn@gmail.com>
2011-10-24 18:51:29 +0100

    a

--- srs_semantics_db/src/srs_semantics_db.py
+++ srs_semantics_db/src/srs_semantics_db.py
@@ -1,20 +1,86 @@
 #!/usr/bin/env python
 
-import roslib
-roslib.load_manifest('json_prolog')
+import roslib; roslib.load_manifest('srs_semantics_db'); roslib.load_manifest('json_prolog')
 import rospy
 import json_prolog
+from srs_semantics_db.srv import *
+from srs_semantics_db.msg import *
+
+class SRSSemanticsDB:
+
+    def __init__(self):
+        print ''
+        rospy.init_node('test_json_prolog')
+    
+        self.prolog = json_prolog.Prolog()
+        query = self.prolog.query("member(A, [1,2,3,4]), B = ['x', A]")
+    
+        for sol in query.solutions():
+            print 'Found solution. A = %s, B = %s' %(sol['A'], sol['B'])
+        query.finish()
+        
+        query = self.prolog.query("owl_subclass_of(A, knowrob:'FoodOrDrink').")
+        for sol in query.solutions():
+            print 'Found solution. A = %s' %(sol['A'])
+
+        query.finish()
+    
+        self.insert_object_in_ontology()
+        self.instances_of_class()
+        rospy.spin()
+
+    #############################################
+    def handle_insert_object_in_ontology(self, req):
+        print 'insert an object into the ontology'
+        print req.object.object_id
+        print req.object.name
+        return InsertObjectResponse(True)
+    
+    def insert_object_in_ontology(self):
+        #rospy.init_node('insert_object_in_ontology_server')
+        s = rospy.Service('insert_object_in_ontology', InsertObject, self.handle_insert_object_in_ontology)
+        
+        print "Ready to insert"
+
+    #############################################
+    def handle_instances_of_class(self, class_info):
+        print 'list all instances of the same class class_info'
+        print class_info.tbox_class.class_id
+        print class_info.tbox_class.name
+        ret = list()
+        ret.append(srs_semantics_db.msg.ABoxObject())
+
+        ### Query... Example here only. need to change
+        query = self.prolog.query("owl_subclass_of(A, knowrob:'FoodOrDrink').")
+        for sol in query.solutions():
+            print 'Found solution. A = %s' %(sol['A'])
+        query.finish()
+
+        
+        return ListInstancesOfClassResponse(ret)
+    
+    def instances_of_class(self):
+        s = rospy.Service('list_all_instances_of_class', ListInstancesOfClass, self.handle_instances_of_class)
+        print 'ready to list instances of one class'
+            
+    #############################################
+"""
+    def handle_instance_info(self, object_id):
+        print ''
+        print class_info.tbox_class.class_id
+        print class_info.tbox_class.name
+        ret = list()
+        ret.append(srs_semantics_db.msg.ABoxObject())
+        
+        return ListInstancesOfClassResponse(ret)
+    
+    def instances_of_class(self):
+        s = rospy.Service('get_instance_info', ListInstancesOfClass, handle_instance_info)
+        print 'ready to list instances of one class'
+"""
+
+
+#############################################
 
 if __name__ == '__main__':
-    rospy.init_node('test_json_prolog')
-    prolog = json_prolog.Prolog()
-    query = prolog.query("member(A, [1,2,3,4]), B = ['x', A]")
-
-    for sol in query.solutions():
-        print 'Found solution. A = %s, B = %s' %(sol['A'], sol['B'])
-
-    query = prolog.query("owl_subclass_of(A, knowrob:'FoodOrDrink').")
-    for sol in query.solutions():
-        print 'Found solution. A = %s' %(sol['A'])
-    query.finish()
-    
+    a = SRSSemanticsDB()
--- srs_semantics_db/src/srs_semantics_db_client.py
+++ srs_semantics_db/src/srs_semantics_db_client.py
@@ -2,10 +2,10 @@
 import roslib; roslib.load_manifest('srs_semantics_db')
 
 import sys
-
 import rospy
 from srs_semantics_db.srv import *
 from srs_semantics_db.msg import *
+
 def test_semantics_db_insert_object():
     rospy.wait_for_service('insert_object_in_ontology')
     try:
@@ -23,8 +23,22 @@ def test_semantics_db_insert_object():
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
+def test_semantics_db_list_objects():
+    rospy.wait_for_service('list_all_instances_of_class')
+    try:
+        list_objects = rospy.ServiceProxy('list_all_instances_of_class', ListInstancesOfClass)
+        input2 = srs_semantics_db.msg.TBoxObject()
+        input2.class_id = 1
+        input2.name = 'tbox class'
+        input2.description = 'class description'
+        ret = list_objects(input2)
+        return ret
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
 def usage():
-    return "%s [x y]"%sys.argv[0]
+    return "......."
 
 if __name__ == "__main__":
     test_semantics_db_insert_object()
+    test_semantics_db_list_objects()

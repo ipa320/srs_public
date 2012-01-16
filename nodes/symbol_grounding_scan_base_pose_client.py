f9156a18b59a2755a510976ee7aba471458cac6b
beds-beisheng <beisheng.liu@beds.ac.uk>
2012-01-16 18:12:35 +0000

    scan pose service update

--- nodes/symbol_grounding_scan_base_pose_client.py
+++ nodes/symbol_grounding_scan_base_pose_client.py
@@ -40,16 +40,16 @@ if __name__ == "__main__":
 	
 	parent_obj_geometry = SRSSpatialInfo()
 	
-	parent_obj_geometry.pose.position.x = workspace_info.objectsInfo[0].pose.position.x
-	parent_obj_geometry.pose.position.y = workspace_info.objectsInfo[0].pose.position.y
-	parent_obj_geometry.pose.position.z = workspace_info.objectsInfo[0].pose.position.z
-	parent_obj_geometry.pose.orientation.x = workspace_info.objectsInfo[0].pose.orientation.x
-	parent_obj_geometry.pose.orientation.y = workspace_info.objectsInfo[0].pose.orientation.y
-	parent_obj_geometry.pose.orientation.z = workspace_info.objectsInfo[0].pose.orientation.z
-	parent_obj_geometry.pose.orientation.w = workspace_info.objectsInfo[0].pose.orientation.w
-	parent_obj_geometry.l = workspace_info.objectsInfo[0].l
-	parent_obj_geometry.w = workspace_info.objectsInfo[0].w
-	parent_obj_geometry.h = workspace_info.objectsInfo[0].h
+	parent_obj_geometry.pose.position.x = workspace_info.objectsInfo[5].pose.position.x
+	parent_obj_geometry.pose.position.y = workspace_info.objectsInfo[5].pose.position.y
+	parent_obj_geometry.pose.position.z = workspace_info.objectsInfo[5].pose.position.z
+	parent_obj_geometry.pose.orientation.x = workspace_info.objectsInfo[5].pose.orientation.x
+	parent_obj_geometry.pose.orientation.y = workspace_info.objectsInfo[5].pose.orientation.y
+	parent_obj_geometry.pose.orientation.z = workspace_info.objectsInfo[5].pose.orientation.z
+	parent_obj_geometry.pose.orientation.w = workspace_info.objectsInfo[5].pose.orientation.w
+	parent_obj_geometry.l = workspace_info.objectsInfo[5].l
+	parent_obj_geometry.w = workspace_info.objectsInfo[5].w
+	parent_obj_geometry.h = workspace_info.objectsInfo[5].h
 
 	furniture_geometry_list = list()
 	furniture_geometry_list = workspace_info.objectsInfo

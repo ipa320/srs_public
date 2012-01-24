31e5b660ede5842f07e43b02de97fa992157efec
Alex Noyvirt <NoyvirtA@cf.ac.uk>
2012-01-24 16:04:57 +0000

    ui test client added

---
+++ srs_decision_making/ui_test_client.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+clear
+
+echo "**** This is a test client that simulates the user intervention through Ipad   ****"
+echo "It listens on /DM_UI/interface_cmd until it receives a suitable command and then sends a reply on /DM_UI/interface_cmd_response simulating the actions of the user on the Ipad"
+echo
+
+
+
+
+
+
+while [ true ]; do
+
+echo
+
+echo "Waiting for a command on /DM_UI/interface_cmd... "
+
+
+ echo
+
+ if [ $(rostopic echo -n 1 /DM_UI/interface_cmd | grep command| cut -b 10-17) = "continue" ]; then
+    `rostopic pub -1 /DM_UI/interface_cmd_response srs_msgs/UI_DMresp  "continue" "sssss" 5 > /dev/null 2>&1` 
+ else 
+    `rostopic pub -1 /DM_UI/interface_cmd_response srs_msgs/UI_DMresp "move" "home" 6 > /dev/null 2>&1`
+ fi
+
+ echo reply sent 
+ echo
+ echo
+ 
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+

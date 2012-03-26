36f7dd1ed3c742095a1b942abf6c3292eda54a3f
mrodriguez-robotnik <mrodriguez@robotnik.es>
2012-03-26 12:39:42 +0200

    Last version

--- srs_grasping/src/get_grasp_configurations.py
+++ srs_grasping/src/get_grasp_configurations.py
@@ -1,4 +1,59 @@
 #!/usr/bin/python
+#################################################################
+##\file
+#
+# \note
+#   Copyright (c) 2012 \n
+#   Robotnik Automation SLL \n\n
+#
+#################################################################
+#
+# \note
+#   Project name: srs
+# \note
+#   ROS stack name: srs
+# \note
+#   ROS package name: srs_grasping
+#
+# \author
+#   Author: Manuel Rodríguez, email:mrodriguez@robotnik.es
+# \author
+#   Supervised by: Manuel Rodríguez, email:mrodriguez@robotnik.es
+#
+# \date Date of creation: March 2012
+#
+# \brief
+#   Implements a service that returns all the grasping configurations for a given object_id.
+#
+#################################################################
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     - Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer. \n
+#     - Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution. \n
+#     - Neither the name of the Robotnik Automation SLL nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission. \n
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License LGPL as 
+# published by the Free Software Foundation, either version 3 of the 
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License LGPL for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public 
+# License LGPL along with this program. 
+# If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################
 
 import roslib
 roslib.load_manifest('srs_grasping')

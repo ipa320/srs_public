7199bd06f4fa339926710871a4ba9efd0072a2bb
jizecn <jizecn@gmail.com>
2012-04-13 12:25:51 +0100

    srs_shared_param minor changes

--- srs_shared_params/src/test_get_param.py
+++ srs_shared_params/src/test_get_param.py
@@ -58,18 +58,18 @@ import sys
 import rospy
 
 def get_params():
-    a = rospy.get_param("/dictionary/a")
+    a = rospy.get_param("/project")
     print a
     f = rospy.get_param("/float")
     print f
-    d1 = rospy.get_param('/dictionary/a')    
-    print d1
+    #d1 = rospy.get_param('/dictionary/a')    
+    #print d1
     #default_param = rospy.get_param('default_param', 'default_value')
 
     # fetch a group (dictionary) of parameters
-    d2 = rospy.get_param('/dictionary')
-    a, c = d2['a'], d2['c']
-    print a, c
+    d2 = rospy.get_param('/nav_params')
+    a = d2['max_dis_linear_nav']
+    print a
 
 if __name__ == "__main__":
     get_params()

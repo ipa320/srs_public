fbd94ac148e49b190a601fd43d5691060adcbceb
Heiko Hoennige <heiko.hoennige@googlemail.com>
2012-04-26 13:07:11 +0100

    modified:   srs_assisted_detection/cmake_install.cmake 	modified:   srs_states/src/assisted_detection_states.py

--- srs_assisted_detection/cmake_install.cmake
+++ srs_assisted_detection/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/srs/git/care-o-bot/srs_public/srs_assisted_detection
+# Install script for directory: /home/heiko/git/srs_public/srs_assisted_detection
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/srs/git/care-o-bot/srs_public/srs_assisted_detection/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/heiko/git/srs_public/srs_assisted_detection/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/srs/git/care-o-bot/srs_public/srs_assisted_detection/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/heiko/git/srs_public/srs_assisted_detection/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
--- srs_states/src/assisted_detection_states.py
+++ srs_states/src/assisted_detection_states.py
@@ -17,9 +17,19 @@ from shared_state_information import *
 from eval_objects import *
 from cob_3d_mapping_msgs.msg import *
 
+from srs_grasping.srv import *
+from geometry_msgs.msg import *
+
+from sensor_msgs.msg import *
+from srs_asisted_detection.srv import *
+from geometry_msgs.msg import *
 ## Detect state
 #
 # This state will try to detect an object.
+outcome = ''
+outcome2 = ''
+call=false
+call2=false
 
 
 class detect_object_assited(smach.State):
@@ -31,8 +41,6 @@ class detect_object_assited(smach.State):
             output_keys=['object_list'])
 
         self.object_list = DetectionArray()
-        self.max_retries = max_retries
-        self.retries = 0
         self.object_name = object_name
         self.srv_name_object_detection = '/object_detection/detect_object'
 
@@ -49,7 +57,93 @@ class detect_object_assited(smach.State):
 
 
     def execute(self, userdata):
-        pass
+        if self.preempt_requested():
+            self.service_preempt()
+            return 'preempted'
+                
+        global s
+        s = rospy.Service('assisted_detection', UiDetector, detectObjectSrv)
+        rospy.loginfo("Assisted Detection ready.")
+        s.spin()
+        if (call):
+            return outcome
+    
+    def detectObjectSrv(self,req):
+       
+
+        # move sdh as feedback
+        sss.move("sdh","cylclosed",False)
+
+        # make the robot ready to inspect the scene
+        if self.retries == 0: # only move arm, sdh and head for the first try
+            sss.say([current_task_info.speaking_language['Search'] + object_name + "."],False)
+            handle_arm = sss.move("arm","folded-to-look_at_table",False)
+            handle_torso = sss.move("torso","shake",False)
+            handle_head = sss.move("head","back",False)
+
+            if self.preempt_requested():
+                self.service_preempt()
+                #handle_base.set_failed(4)
+                handle_arm.client.cancel_goal()
+                handle_torso.client.cancel_goal()
+                handle_head.client.cancel_goal()
+                global outcome
+                outcome= 'preempted'
+            else:
+                handle_arm.wait()
+                handle_head.wait()
+                handle_torso.wait()
+
+        handle_torso = sss.move("torso",self.torso_poses[self.retries % len(self.torso_poses)]) # have an other viewing point for each retry
+
+        # move sdh as feedback
+        sss.move("sdh","home",False)
+
+        # wait for image to become stable
+        sss.sleep(2)
+
+
+        # check if object detection service is available
+        try:
+            rospy.wait_for_service(self.srv_name_object_detection,10)
+        except rospy.ROSException, e:
+            print "Service not available: %s"%e
+            self.retries = 0 # no object found within min_dist start value
+            global outcome
+            outcome= 'failed'
+
+        # call object detection service
+        try:
+            detector_service = rospy.ServiceProxy(self.srv_name_object_detection, DetectObjects)
+            req = DetectObjectsRequest()
+            req.object_name.data = userdata.object_name
+            res = detector_service(req)
+            userdata.object_list=res.object_list
+            
+            global outcome
+            outcome= 'succeeded'
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+            global outcome
+            outcome= 'failed'
+
+        global detector_response
+        if len(resp1.object_list.detections) > 0:
+            detector_response.object_list.header=resp1.object_list.header
+            for x in range(len(resp1.object_list.detections)):
+                detector_response.object_list.detections.insert(x,resp1.object_list.detections[x])
+        global call
+        call=true
+        s.shutdown()
+        userdata.object_list=detector_response.object_list
+        return detector_response  
+    
+
+
+       
+
+
+     
 
 
 
@@ -61,4 +155,83 @@ class user_intervention_on_detection(smach.State):
                              output_keys=['object','object_pose','bb_pose'])
         
     def execute(self, userdata):
-        pass
+        if self.preempt_requested():
+            self.service_preempt()
+            return 'preempted'
+        global s
+        global s2
+        s = rospy.Service('assisted_answer', UiAnswer, answerObjectSrv)
+        s2 = rospy.Service('assisted_BBmove', BBMove, moveBBSrv)
+
+        rospy.loginfo("Assisted answer ready.")
+        s.spin()
+        s2.spin()
+        if(call2):
+            return outcome2
+        
+    def answerObjectSrv(self,req):    
+        call2=true
+        rospy.loginfo("Get Object information")
+        answer=UiAnswerResponse()
+        
+        if(req.action=='give up'):
+            outcome2 = 'give up'
+            answer.message.data='process stopped'
+            return answer
+            
+            
+
+        #save
+        
+            
+       
+        #get position from good object
+        pose=Pose()
+        pose.position.x=userdata.object_list.detections[req.id].pose.pose.position.x
+        pose.position.y=userdata.object_list.detections[req.id].pose.pose.position.y
+        pose.position.z=userdata.object_list.detections[req.id].pose.pose.position.z
+        pose.orientation.x=userdata.object_list.detections[req.id].pose.pose.orientation.x
+        pose.orientation.y=userdata.object_list.detections[req.id].pose.pose.orientation.y
+        pose.orientation.z=userdata.object_list.detections[req.id].pose.pose.orientation.z
+        pose.orientation.w=userdata.object_list.detections[req.id].pose.pose.orientation.w
+        
+        
+        userdata.object_pose=pose
+        userdata.object=userdata.object_list.detections[req.id]
+        
+       
+        #global action
+        #default for user
+        #action='grasp'
+    
+        s.shutdown()
+        s2.shutdown()
+        outcome2 = 'succeeded'
+        answer.message.data='Action is running'
+        return answer
+    
+            
+        
+        
+    def moveBBSrv(self,req):
+        rospy.loginfo("Get BB information")
+        call2=true
+        #BBmove service base and then movement
+        moveBB=BBMoveResponse()
+        moveBB.message.data='moving to better position'
+        
+        #
+        
+        #service call to get better position 
+        pose=Pose()
+        pose.position.x=1
+        pose.position.y=2
+        pose.position.z=3
+        userdata.bbpose=pose
+        
+        outcome2='bb_move'
+        
+        s.shutdown()
+        s2.shutdown()
+        return moveBB
+    

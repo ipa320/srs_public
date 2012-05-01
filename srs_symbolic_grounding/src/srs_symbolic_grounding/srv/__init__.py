e83b5ceeed9625dfdcc2ad848996e1b2cde5c030
beds-beisheng <beisheng.liu@beds.ac.uk>
2012-05-01 18:17:19 +0100

    update

--- srs_symbolic_grounding/src/srs_symbolic_grounding/srv/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._GetRobotBasePose import *
-from ._SymbolGroundingPrimitiveBasePose import *
-from ._SymbolGroundingScanBasePose import *
-from ._SymbolGroundingGraspBasePoseExperimental import *
-from ._SymbolGroundingGraspBasePose import *
-from ._SymbolGroundingExploreBasePose import *
-from ._GetWorkspaceOnMap import *
--- srs_symbolic_grounding/srv_gen/lisp/_package_GetRobotBasePose.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package srs_symbolic_grounding-srv)
-(cl:export '(RB_POSE-VAL
-          RB_POSE
-))

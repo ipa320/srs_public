c6df7de5f34557f740cb7f6d2d773cf6ab6bfbb1
cu-noyvirt <NoyvirtA@cf.ac.uk>
2012-05-03 11:20:49 +0100

    human sensing package added

---
+++ srs_human_sensing/src/srs_human_sensing/hs.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('srs_human_sensing')
+import rospy
+import math
+from std_msgs.msg import String
+from sensor_msgs.msg import PointCloud
+from geometry_msgs.msg import Point32
+from cob_people_detection_msgs.msg import PeopleDetectionArray
+import tf
+from std_msgs.msg import Header
+
+
+def callback_LD(data):
+    rospy.loginfo("I heard from LD:")
+    leg_detections = data.points
+    global legs 
+    legs = []
+    for i in range (len(leg_detections)):
+       legs.append(leg_detections[i])
+       print leg_detections[i].x,",",leg_detections[i].y
+    
+    match_detections()
+
+
+def callback_FD(data):
+    rospy.loginfo("I heard from FD:")
+    face_detections = data.detections
+    
+    
+    global faces 
+    faces = []
+
+    for i in range (len(face_detections)):
+       
+    
+
+       try:
+
+          detection1 = tflistener.transformPose('map', face_detections[i].pose)        
+          newpoint = Point32(detection1.pose.position.x,detection1.pose.position.y,detection1.pose.position.z)
+          faces.append(newpoint)
+
+       except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException): 
+         print "tf transform error"
+   
+    match_detections () 
+
+def match_detections ():
+    distances = [] 
+    global legs
+    global faces
+    global pub
+
+    pc = PointCloud()
+    pc.header = Header(0,rospy.get_rostime(),'/map')
+    pc.points = []
+    pc.points.append(Point32(1.,1.,1.)) 
+    pc.points.append(Point32(1.,1.,1.))
+    pc.points.append(Point32(1.,1.,1.))
+
+    for l in range (len(legs)):
+         distancecol = []
+         for m in range (len(faces)):
+           # measure the distance between the detections and store them
+              distancecol.append (sqrt((faces[m][0]-legs[l][0])**2+(faces[m][1]-legs[l][1])**2))
+         distances.append (distancecol) 
+         pc.points.append(Point32(1.,1.,0.)) 
+    print "distances"
+    print distances
+     
+    
+    
+    pc = PointCloud()
+    pc.header = Header(0,rospy.get_rostime(),'/map')
+    
+    
+    pub.publish (pc)
+      
+
+def listener():
+    
+    rospy.Subscriber("leg_detections_cloud", PointCloud , callback_LD)
+    rospy.Subscriber("/cob_people_detection/face_position_array", PeopleDetectionArray , callback_FD)
+    rospy.spin()
+
+if __name__ == '__main__':
+    print "Listening ..."
+
+
+    rospy.init_node('SRS_HS_listener', anonymous=True)
+    legs = []
+    faces = []
+    tflistener = tf.TransformListener()
+    tflistener.waitForTransform("/head_cam3d_link", "/map", rospy.Time(), rospy.Duration(5.0))
+    pub = rospy.Publisher('people_detections_cloud', PointCloud)
+    
+    listener()

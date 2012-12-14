c6c62615bc470d04e28b891f1345fd953b8392c3
but-spanel <spanel@fit.vutbr.cz>
2012-12-14 12:36:32 +0100

    Max distance filter

--- srs_env_model_ui/src/but_rviz_display/point_cloud_display.cpp
+++ srs_env_model_ui/src/but_rviz_display/point_cloud_display.cpp
@@ -63,6 +63,7 @@ CButPointCloud::CButPointCloud( const std::string& name, rviz::VisualizationMana
     , draw_view_frustum_( false )
     , view_frustum_depth_( 4 )
     , cull_view_frustum_( false )
+	, max_distance_( 10 )
 {
     // Connect to the input - point cloud advertiser
     tf_filter_.connectInput(sub_);
@@ -310,7 +311,7 @@ void CButPointCloud::incomingCloudCallback(const sensor_msgs::PointCloud2ConstPt
     // VIEW FRUSTUM filtering
     bool culling_enabled = false;
     Eigen::Vector3f n[4], cam;
-    float d[4], max_depth = 10.0f;
+    float d[4], max_depth = 10.0f, max_distance = 10.0f;
     if( caminfo_ && cull_view_frustum_ )
     {
         boost::mutex::scoped_lock lock(caminfo_mutex_);
@@ -319,6 +320,7 @@ void CButPointCloud::incomingCloudCallback(const sensor_msgs::PointCloud2ConstPt
         {
             culling_enabled = true;
             max_depth = view_frustum_depth_;
+            max_distance = max_distance_;
 
             // Transform points defining the view frustum into the correct frame
             geometry_msgs::PointStamped point, tl, tr, bl, br, camera;
@@ -402,15 +404,21 @@ void CButPointCloud::incomingCloudCallback(const sensor_msgs::PointCloud2ConstPt
         {
             if( culling_enabled )
             {
-                Eigen::Vector3f p(x, y, z);
+            	Eigen::Vector3f p(x, y, z);
+            	Eigen::Vector3f aux = p - cam;
+                float dist = aux.norm();
+                if( dist > max_distance )
+                {
+                    continue;
+                }
+
                 float e1 = n[0].dot(p) + d[0];
                 float e2 = n[1].dot(p) + d[1];
                 float e3 = n[2].dot(p) + d[2];
                 float e4 = n[3].dot(p) + d[3];
                 if( e1 > 0.0f && e2 > 0.0f && e3 > 0.0f && e4 > 0.0f )
                 {
-                    p -= cam;
-                    if( p.norm() < max_depth )
+                    if( dist < max_depth )
                     {
                         continue;
                     }
@@ -680,6 +688,16 @@ void CButPointCloud::createProperties()
             "Controls weather the points inside the view frustum are removed or not.");
 
 
+    max_distance_property_ = property_manager_->createProperty<rviz::FloatProperty> (
+            "Max Distance", property_prefix_, boost::bind(
+            &CButPointCloud::getMaxDistance, this), boost::bind(
+            &CButPointCloud::setMaxDistance, this, _1), parent_category_,
+            this);
+    setPropertyHelpText(
+            max_distance_property_,
+            "Maximal allowed distance of points from the robot.");
+
+
     // Polygon position
     position_property_ = property_manager_->createProperty<rviz::Vector3Property> (
             "Position", property_prefix_, boost::bind(
@@ -736,6 +754,17 @@ void CButPointCloud::setCullViewFrustum(bool value)
     cull_view_frustum_ = value;
 
     propertyChanged(cull_view_frustum_property_);
+
+    causeRender();
+}
+
+
+void CButPointCloud::setMaxDistance(float value)
+{
+    // set internal variable
+    max_distance_ = value;
+
+    propertyChanged(max_distance_property_);
 
     causeRender();
 }

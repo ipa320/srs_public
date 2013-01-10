020dd93f8d956b1ba528899a4190e265cdbf615d
ipa-fmw <florian.weisshardt@ipa.fraunhofer.de>
2013-01-10 15:44:02 +0100

    training for srs

---
+++ srs_training/src/move_box.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('srs_training')
+import rospy
+from geometry_msgs.msg import Twist
+import tf
+
+def callback(msg):
+    print msg
+    
+    
+
+def move_box():
+    rospy.init_node('move_box')
+    rospy.Subscriber("/base_controller/command", Twist, callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    move_box()

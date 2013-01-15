721641dc2352b171792beffb68e6d97430d059c9
Thiago de Freitas <thiagodefreitas@gmail.com>
2013-01-15 13:32:31 +0100

    2nd SRS Training - Controlling movement inside gazebo and rviz

--- srs_training/src/move_box.py
+++ srs_training/src/move_box.py
@@ -1,8 +1,66 @@
 #!/usr/bin/env python
+
+#################################################################
+##\file
+#
+# \note
+# Copyright (c) 2013 \n
+# Fraunhofer Institute for Manufacturing Engineering
+# and Automation (IPA) \n\n
+#
+#################################################################
+#
+# \note
+# Project name: Care-O-bot Research
+# \note
+# ROS package name: 
+#
+# \author
+# Author: Thiago de Freitas Oliveira Araujo, 
+# email:thiago.de.freitas.oliveira.araujo@ipa.fhg.de
+# \author
+# Supervised by: Florian Weisshardt, email:florian.weisshardt@ipa.fhg.de
+#
+# \date Date of creation: January 2013
+#
+# \brief
+# This is part of the SRS training package
+#
+#################################################################
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# - Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer. \n
+# - Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution. \n
+# - Neither the name of the Fraunhofer Institute for Manufacturing
+# Engineering and Automation (IPA) nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission. \n
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License LGPL as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License LGPL for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License LGPL along with this program.
+# If not, see < http://www.gnu.org/licenses/>.
+#
+#################################################################
 import roslib; roslib.load_manifest('srs_training')
 import rospy
 from geometry_msgs.msg import Twist
 import tf
+from math import *
 
 
 class move_box():
@@ -13,37 +71,53 @@ class move_box():
         self.reference_frame = "box1"
         self.target_frame = "base_link"
         rospy.Subscriber("/base_controller/command", Twist, self.callback)
-    
-    def callback(self,msg):
-        print msg
         
-        self.tf_broadcaster = tf.TransformBroadcaster()
+        self.x = 0.
+        self.y = 0.
+        self.z = 0.2
+        self.r = 0.
+        self.p = 0.
+        self.t = 0.
         
-        self.tf_listener.waitForTransform(self.reference_frame, self.target_frame, rospy.Time(0), rospy.Duration(3.0))
-        trans, rot = self.tf_listener.lookupTransform(self.reference_frame, self.target_frame, rospy.Time(0))
-        
-        print trans, rot
-        
-        x = msg.linear.x + trans[0]
-        y = msg.linear.y + trans[1]
-        theta = msg.angular.z + rot[2]
-        
-        print x,y, theta
-               
-        self.tf_broadcaster.sendTransform((x, y, 0),
-            tf.transformations.quaternion_from_euler(0, 0, theta),
+        rospy.sleep(2)
+        self.tf_broadcaster.sendTransform((self.x, self.y, self.z),
+            tf.transformations.quaternion_from_euler(self.r, self.p, self.t),
             rospy.Time.now(),
             self.reference_frame,
             self.target_frame)
+
+        self.current_time = rospy.get_time()
+        self.last_time = rospy.get_time()
     
-    # TODO:
-        # get TF
-        # add step based on velocity and time
-        # publish TF
+    def callback(self,msg):
+   
+        dt = self.current_time - self.last_time
+        
+        self.x += msg.linear.x*cos(self.t)-msg.linear.y*sin(self.t)
+        self.y += msg.linear.x*sin(self.t)+msg.linear.y*cos(self.t)
+        self.t += msg.angular.z
+        
+        self.tf_broadcaster.sendTransform((self.x, self.y, self.z),
+            tf.transformations.quaternion_from_euler(self.r, self.p, self.t),
+            rospy.Time.now(),
+            self.reference_frame,
+            self.target_frame)
+
+        
+        self.last_time = self.current_time
         
 
 if __name__ == '__main__':
     rospy.init_node('move_box')
     mb = move_box()
-    rospy.spin()
+    rate = rospy.Rate(50) 
+    while not rospy.is_shutdown():
+        mb.current_time = rospy.get_time()
+        mb.tf_broadcaster.sendTransform((mb.x, mb.y, mb.z),
+            tf.transformations.quaternion_from_euler(mb.r, mb.p, mb.t),
+            rospy.Time.now(),
+            mb.reference_frame,
+            mb.target_frame)
+        mb.last_time = mb.current_time
+        rate.sleep()
     

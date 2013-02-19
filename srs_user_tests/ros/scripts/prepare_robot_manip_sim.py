7927b2d6a685d80355a6b07bcc408be176dc92d4
but-spanel <spanel@fit.vutbr.cz>
2013-02-19 12:55:01 +0100

    Launch files preparation for user tests

--- srs_user_tests/ros/scripts/prepare_robot_manip_sim.py
+++ srs_user_tests/ros/scripts/prepare_robot_manip_sim.py
@@ -28,6 +28,7 @@
 import roslib; roslib.load_manifest('srs_user_tests')
 import rospy
 from simple_script_server import *
+from std_msgs.msg import Empty as EmptyMsg
 
 sss = simple_script_server()
 
@@ -36,6 +37,14 @@ def main():
     
     rospy.init_node('prepare_robot_for_manip_test_node')
     rospy.sleep(2)
+    
+    sim = rospy.get_param('/use_sim_time')
+    
+    if sim is True:
+    
+      rospy.loginfo('Waiting until end of spawning...')
+      rospy.wait_for_message('/sim_spawned',EmptyMsg)
+      rospy.sleep(5)
     
     sss.init('head', True)
     sss.init('tray', True)
@@ -55,8 +64,19 @@ def main():
     
     
     rospy.loginfo('Moving torso...')
-    #sss.move('torso', 'back', True)
-    sss.move('torso', 'home', True)
+    sss.move('torso', 'back', True)
+    #sss.move('torso', 'home', True)
+    
+    if sim is True:
+    
+      rospy.loginfo('Running in simulation, publishing to /sim_robot_init topic')
+      
+      pub = rospy.Publisher('/sim_robot_init', EmptyMsg,latch=True)
+      pub.publish(EmptyMsg())
+      pub.publish(EmptyMsg())
+      pub.publish(EmptyMsg())
+      
+      rospy.spin()
 
 
 if __name__ == '__main__':
